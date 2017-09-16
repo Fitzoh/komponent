49037a28f47ab6a416da73b4566dfc939445f3e0
Jordan Demeulenaere <jordan@jarvis.home>
2017-09-16 18:25:55 +0200

    Add drawer

--- src/main/kotlin/komponent/polymer/element/AppDrawerElement.kt
+++ src/main/kotlin/komponent/polymer/element/AppDrawerElement.kt
@@ -3,6 +3,24 @@ package komponent.polymer.element
 import komponent.core.createElement
 import org.w3c.dom.HTMLElement
 
-external abstract class AppDrawerElement : PolymerElement
+external abstract class AppDrawerElement : PolymerElement {
+
+	var align: String
+
+	var disableSwipe: Boolean
+
+	var noFocusTrap: Boolean
+
+	var persistent: Boolean
+
+	val position: String
+
+	var swipeOpen: Boolean
+
+	var transitionDuration: Int
+
+}
 
 fun HTMLElement.appDrawer(init: (AppDrawerElement.() -> Unit)? = null) = createElement("app-drawer", this, init)
+
+fun AppDrawerElement.opened() = mutableProperty<Boolean>("opened")
